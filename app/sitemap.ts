bc2fbc3ec2322bdd9ca4084460b866d473388b69
won-hope <hopedad.0604@gmail.com>
2026-01-20 00:15:44 +0900

    fix : 수정완료

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next'
-import { allBlogs } from 'contentlayer/generated'
 import siteMetadata from '@/data/siteMetadata'
+import { allBlogs } from 'contentlayer/generated'
 
 export const dynamic = 'force-static'
 
